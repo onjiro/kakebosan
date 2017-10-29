8e803570012b8eee29309fdd806be12495f2fb35
onjiro <onjiro.mohyahya@gmail.com>
2017-10-29 22:05:20 +0900

    Set up vue.js

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,39 +1,59 @@
+var path = require("path");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   devtool: "source-map",
   entry: {
-    app: ["./css/app.scss", "./js/app.jsx"]
+    app: ["./css/app.scss", "./js/app.js"]
   },
   output: {
-    path: "../priv/static",
+    path: path.resolve(__dirname, '../priv/static'),
     filename: "js/app.js"
   },
   resolve: {
-    modulesDirectories: [ "node_modules", __dirname + "/js" ],
-    extensions: ["", ".js", ".jsx"]
+    extensions: [".js", ".vue"]
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: ["babel"],
-      include: __dirname,
-      query: {
-        presets: ["react", "es2015"]
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: 'css-loader!sass-loader'
+          },
+          transformToRequire: {
+            video: 'src',
+            source: 'src',
+            img: 'src',
+            image: 'xlink:href'
+          }
+        }
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        include: __dirname
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
       }
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract("style", "css")
-    }, {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract("style", "css!sass?includePaths[]=" + __dirname +  "/node_modules")
-    }]
+    ]
   },
   plugins: [
     new ExtractTextPlugin("css/app.css"),
     new CopyWebpackPlugin([{ from: "./static" }]),
-    new CopyWebpackPlugin([{ from: "./node_modules/bootstrap/dist" }]),
-    new CopyWebpackPlugin([{ from: "./node_modules/react-select/dist", to: "css/" }])
   ]
 };
