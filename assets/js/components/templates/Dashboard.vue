961fc5be393a406fe60e796bdb531ec4f186e33d
onjiro <onjiro.mohyahya@gmail.com>
2018-08-03 22:36:23 +0900

    ダッシュボード画面での履歴を日付の新しい順に変更

--- assets/js/components/templates/Dashboard.vue
+++ assets/js/components/templates/Dashboard.vue
@@ -58,7 +58,7 @@ export default {
         }),
         axios.get('api/items')
       ]).then(([{ data: { data: transactions }}, { data: { data: items }}]) => {
-        this.transactions = transactions;
+        this.transactions = transactions.reverse();
         this.items = items;
       });
     },
